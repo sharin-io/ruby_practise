a9920bd785cc8ec19c9c7839df820fc2fab11f91
Sharin <shinthantthiri266@gmail.com>
2024-06-02 01:08:12 +0800

    Changes to names.rb

--- intro/02_control_flow/names.rb
+++ intro/02_control_flow/names.rb
@@ -5,4 +5,6 @@ name3 = "Beric Dondarrion"
 name4 = "Melisandre"
 
 # Your code goes here
-
+sum_length = name1.length + name2.length + name3.length + name4.length
+avg_length = sum_length.to_f / 4
+puts avg_length.to_f
